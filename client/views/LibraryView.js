f94d90c7a5cead559d0d9b17dcf755df0573830b
Blake West <bwest87@gmail.com>
2013-11-23 18:12:13 -0800

    refactored so it could be more consistent with player view

--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -3,12 +3,16 @@ var LibraryView = Backbone.View.extend({
 
   tagName: "table",
 
+  initialize: function() {
+    this.render();
+  },
+
   render: function(){
     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
     // see http://api.jquery.com/detach/
     this.$el.children().detach();
 
-    return this.$el.html('<th>Library</th>').append(
+    this.$el.html('<th>Library</th>').append(
       this.collection.map(function(song){
         return new LibraryEntryView({model: song}).render();
       })
