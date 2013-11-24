420bc9aa9c44fc99bdf294a7f75e3335ebaba92a
Blake West <bwest87@gmail.com>
2013-11-23 18:11:53 -0800

    refactored to be consistent with how we're handling library view

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -3,6 +3,7 @@ var AppView = Backbone.View.extend({
 
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
+    this.libraryView = new LibraryView({collection: this.model.get('library')});
 
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
@@ -12,7 +13,7 @@ var AppView = Backbone.View.extend({
   render: function(){
     return this.$el.html([
       this.playerView.$el,
-      new LibraryView({collection: this.model.get('library')}).render()
+      this.libraryView.$el
     ]);
   }
 
